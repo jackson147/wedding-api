b2f14022effe914750f51e749d3472f1fbb96f69
Richard Jackson <jaswahhihi@gmail.com>
2019-07-21 11:18:18 +0100

    Added wait-port call to wait for mongodb to come up before attempting to connect

--- api.js
+++ api.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3001
-
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const waitPort = require('wait-port')
+
+const params = {
+  host: 'mongo',
+  port: 27017,
+};
 
 // use it before all route definitions
 app.use(cors({origin: '*'}));
 app.use(bodyParser.json())
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://mongo/wedding');
+waitPort(params)
+  .then((open) => {
+    if (open){
+        mongoose.connect('mongodb://mongo/wedding');
+        require('./routes/registry')(app);
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    }
+    else console.log('The port did not open before the timeout...');
+  })
+  .catch((err) => {
+    console.log(`An unknown error occured while waiting for the port: ${err}`);
+  });
 
-require('./routes/registry')(app);
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- api.prod.js
+++ api.prod.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3001
-
 const https = require('https')
 const fs = require('fs')
-
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const waitPort = require('wait-port')
+
+const params = {
+  host: 'mongo',
+  port: 27017,
+};
 
 // use it before all route definitions
 app.use(cors({origin: '*'}));
 app.use(bodyParser.json())
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://mongo/wedding');
-
-require('./routes/registry')(app);
-
-https.createServer({
-	ca: fs.readFileSync('/run/secrets/server.ca'),
-	key: fs.readFileSync('/run/secrets/server.key'),
-	cert: fs.readFileSync('/run/secrets/server.crt')
-}, app)
-.listen(port, function () {
-	console.log(`Example app listening on port ${port}!`)
-})
+waitPort(params)
+  .then((open) => {
+    if (open){
+        mongoose.connect('mongodb://mongo/wedding');
+		require('./routes/registry')(app);
+		https.createServer({
+			ca: fs.readFileSync('/run/secrets/server.ca'),
+			key: fs.readFileSync('/run/secrets/server.key'),
+			cert: fs.readFileSync('/run/secrets/server.crt')
+		}, app)
+		.listen(port, function () {
+			console.log(`Example app listening on port ${port}!`)
+		})
+    }
+    else console.log('The port did not open before the timeout...');
+  })
+  .catch((err) => {
+    console.log(`An unknown error occured while waiting for the port: ${err}`);
+  });
